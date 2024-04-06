cf364d9cd0d8c714736022d0160eb57145ccc0a1
rahul-tripathi <rt84153@gmail.com>
2024-04-06 16:16:21 +0530

    Change constant.

--- src/locals/constant.js
+++ src/locals/constant.js
@@ -1,5 +1,6 @@
 export const constant = {
-    "yatinidhi":"YatiNidhi",
-    "build_your_visions":"Building your visions",
-    "elevate_your_CGD":"Elevate your CGD (City Gas Distribution) and hydrocarbon  projects with our expertise in steel pipeline installation. Our meticulous craftsmanship ensures reliable and efficient transportation of natural gas and hydrocarbons, powering sustainable energy solutions."
-}
+  yatinidhi: "YatiNidhi",
+  build_your_visions: "Building your visions",
+  elevate_your_CGD:
+    "Elevate your CGD (City Gas Distribution) and hydrocarbon  projects with our expertise in steel pipeline installation. Our meticulous craftsmanship ensures reliable and efficient transportation of natural gas and hydrocarbons, powering sustainable energy solutions.",
+};
